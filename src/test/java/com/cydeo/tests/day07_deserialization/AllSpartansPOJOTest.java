fd25e5ece2c19664beaeef66f02274701506f948
nadireirem <nadireulsular@gmail.com>
2022-08-10 22:26:02 -0400

    API

---
+++ src/test/java/com/cydeo/tests/day07_deserialization/AllSpartansPOJOTest.java
@@ -0,0 +1,70 @@
+package com.cydeo.tests.day07_deserialization;
+
+import com.cydeo.pojo.Spartan;
+import com.cydeo.utils.SpartanTestBase;
+import io.restassured.http.ContentType;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AllSpartansPOJOTest extends SpartanTestBase {
+    /**
+     Given accept type is json
+     when I send GET request to /spartans
+     Then status code is 200
+     And content type is json
+     And I can convert json to list of spartan pojos
+     */
+    @Test
+    public void allSpartansToPojoTest() {
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/spartans");
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.contentType());
+
+        //convert response to jsonpath
+        JsonPath jsonPath = response.jsonPath();
+
+        //using jsonPath extract List of spartans/ do deserialization
+        List<Spartan> allSpartans = jsonPath.getList("",Spartan.class);
+
+        System.out.println("count = " + allSpartans.size());
+
+        //first spartan
+        System.out.println("first spartan = " + allSpartans.get(0));
+
+        //using streams: filter and store female spartans into a different list
+        List<Spartan> femaleSpartans = allSpartans.stream()
+                .filter(spartan -> spartan.getGender().equals("Female"))
+                .collect(Collectors.toList());
+
+        System.out.println("femaleSpartans = " + femaleSpartans);
+        System.out.println("femaleSpartans.size() = " + femaleSpartans.size());
+
+        //using streams: filter and store male spartans into a different list
+        //source.stream.filter(condition in lambda).collect(type)
+        long count = allSpartans.stream().filter(sp -> sp.getGender().equals("Male")).count();
+        System.out.println("male spartan count = " + count);
+        List<Spartan> maleSpartans = allSpartans.stream().filter(sp -> sp.getGender().equals("Male"))
+                .collect(Collectors.toList());
+        System.out.println("maleSpartans = " + maleSpartans);
+
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/tests/day07_deserialization/SpartanSearchPOJOTest.java
@@ -0,0 +1,77 @@
+package com.cydeo.tests.day07_deserialization;
+
+import com.cydeo.pojo.Spartan;
+import com.cydeo.pojo.SpartanSearch;
+import com.cydeo.utils.SpartanTestBase;
+import io.restassured.http.ContentType;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanSearchPOJOTest extends SpartanTestBase {
+
+    /**
+     Given accept type is json
+     And query param nameContains value is "e"
+     And query param gender value is "Female"
+     When I send get request to /spartans/search
+     Then status code is 200
+     And content type is Json
+     And json response data is as expected
+     */
+    @Test
+    public void spartanSearchToPOJOTest() {
+        Map<String, String> queryMap = new HashMap<>();
+        queryMap.put("nameContains","e");
+        queryMap.put("gender","Female");
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams(queryMap)
+                .when().get("/spartans/search");
+        response.prettyPrint();
+
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertEquals(ContentType.JSON.toString(), response.contentType());
+
+        //deserialize json to SpartanSearch pojo class
+        SpartanSearch spartanSearch = response.body().as(SpartanSearch.class);
+
+        //total elements
+        System.out.println("total Element = " + spartanSearch.getTotalElement());
+        System.out.println("All spartans = " + spartanSearch.getContent());
+        System.out.println("First spartan info = " + spartanSearch.getContent().get(0));
+
+        //goto to content list of spartans and get index 1 single spartan
+        Spartan secondSpartan = spartanSearch.getContent().get(1);
+        System.out.println("secondSpartan = " + secondSpartan);
+        System.out.println("second spartan name = " + secondSpartan.getName());
+        System.out.println("second spartan id = " + secondSpartan.getId());
+
+        List<Spartan> spartanData = spartanSearch.getContent();
+        System.out.println("second spartan = " + spartanData.get(1));
+
+        //read all names into a list
+        List<String> names = new ArrayList<>();
+        for (Spartan sp : spartanData) {
+            names.add(sp.getName());
+        }
+
+        System.out.println("names = " + names);
+        //using functional programming. stream
+        List<String> allNames = spartanData.stream().map(sp -> sp.getName()).collect(Collectors.toList());
+        System.out.println("allNames = " + allNames);
+    }
+
+}
