308d24f49470026d95dafaf7de9027823056a0f8
nadireirem <nadireulsular@gmail.com>
2022-08-21 19:35:33 -0400

    API

---
+++ src/test/java/com/cydeo/utils/BookItAPITestBase.java
@@ -0,0 +1,29 @@
+package com.cydeo.utils;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+public abstract class BookItAPITestBase {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("bookit.base.url");
+    }
+
+    public static String getAccessToken(String email, String password) {
+        String accessToken = given().accept(ContentType.JSON)
+                .and().queryParam("email", email)
+                .and().queryParam("password", password)
+                .when().get("/sign")
+                .then().assertThat().statusCode(200)
+                .and().extract().path("accessToken");
+
+        assertThat("accessToken is empty or null",accessToken, not(emptyOrNullString()));
+
+        return "Bearer " + accessToken;
+    }
+
+}
